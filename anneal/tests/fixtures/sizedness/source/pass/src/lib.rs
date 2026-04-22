54bd85c25824a1dba5cc245d9358ffbf2b63e91f
Joshua Liebow-Feeser <joshlf@google.com>
2026-04-22 08:56:42 +0000

    [anneal] Replace annotation syntax with verbatim Lean
    gherrit-pr-id: G4ka5fwcrxku4rfg7rjpfswfmnqxqulql

--- anneal/tests/fixtures/sizedness/source/pass/src/lib.rs
+++ anneal/tests/fixtures/sizedness/source/pass/src/lib.rs
@@ -4,7 +4,11 @@ pub struct NamedStruct {
 }
 
 /// ```lean, anneal
-/// context:
 /// derive_sized sizedness_pass.NamedStruct
 /// ```
 pub fn foo(_f: &NamedStruct) {}
+
+/// ```lean, anneal, spec
+/// theorem spec : True := by trivial
+/// ```
+pub fn dummy_force_compile() {}
--- anneal/tests/fixtures/sizedness/source/reject/src/lib.rs
+++ anneal/tests/fixtures/sizedness/source/reject/src/lib.rs
@@ -4,7 +4,11 @@ pub struct NamedDst {
 }
 
 /// ```lean, anneal
-/// context:
 /// derive_sized sizedness_reject.NamedDst
 /// ```
 pub fn foo(_f: &NamedDst) {}
+
+/// ```lean, anneal, spec
+/// theorem spec : True := by trivial
+/// ```
+pub fn dummy_force_compile() {}
