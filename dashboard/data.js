c4db91cb45b40c92f60397e40f1aa3fdf56004fe
github-action-benchmark <github@users.noreply.github.com>
2026-04-28 18:37:56 +0000

    add Docker Build Time (customSmallerIsBetter) benchmark result for f896a6890a1e6feb01d3c26364bf674f0573321e

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777400442975,
+  "lastUpdate": 1777401476832,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -3113,6 +3113,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/google/zerocopy/commit/f896a6890a1e6feb01d3c26364bf674f0573321e"
         },
         "date": 1777399859380,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Build Time",
+            "value": 7,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshlf@users.noreply.github.com",
+            "name": "Joshua Liebow-Feeser",
+            "username": "joshlf"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f896a6890a1e6feb01d3c26364bf674f0573321e",
+          "message": "[anneal][README] Document TCB shrinking (#3326)\n\ngherrit-pr-id: Gy4y7cstkui5s6c7jletzjbu37xtjglxm",
+          "timestamp": "2026-04-28T18:10:09Z",
+          "tree_id": "04fd33df84b5b512246673abd226b3bf491693a7",
+          "url": "https://github.com/google/zerocopy/commit/f896a6890a1e6feb01d3c26364bf674f0573321e"
+        },
+        "date": 1777401474494,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
