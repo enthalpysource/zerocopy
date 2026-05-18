d75ebf058c7f55fe13784f7784702e9caac95097
github-action-benchmark <github@users.noreply.github.com>
2026-05-18 20:35:26 +0000

    add CI Durations (customSmallerIsBetter) benchmark result for a3ad195abf85896be6f07f11acb486ceb7d61900

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779135864014,
+  "lastUpdate": 1779136526153,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -8674,6 +8674,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Total CI Duration (All Steps)",
             "value": 734,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshlf@users.noreply.github.com",
+            "name": "Joshua Liebow-Feeser",
+            "username": "joshlf"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a3ad195abf85896be6f07f11acb486ceb7d61900",
+          "message": "[anneal] README: Explain philosophy w.r.t. typing (#3351)\n\nRelease 0.1.0-alpha.23.\n\ngherrit-pr-id: Gpeqma2krsi7flwaeyjlum23s7znxuhms",
+          "timestamp": "2026-05-18T16:03:33-04:00",
+          "tree_id": "42557b2a78eba0ec7aaa24b045a0f4e098ac18eb",
+          "url": "https://github.com/google/zerocopy/commit/a3ad195abf85896be6f07f11acb486ceb7d61900"
+        },
+        "date": 1779136522997,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Pull Time",
+            "value": 88,
+            "unit": "seconds"
+          },
+          {
+            "name": "Test Time",
+            "value": 583,
+            "unit": "seconds"
+          },
+          {
+            "name": "Total CI Duration (All Steps)",
+            "value": 757,
             "unit": "seconds"
           }
         ]
