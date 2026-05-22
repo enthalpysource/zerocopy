481b46ad4767ec307801fc21c37df1a945a570f3
github-action-benchmark <github@users.noreply.github.com>
2026-05-22 22:10:21 +0000

    add Docker Build Time (customSmallerIsBetter) benchmark result for d48a2766ccaffa7f283474bdfd204c0612fd7272

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779487815601,
+  "lastUpdate": 1779487821055,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -6575,6 +6575,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "Docker Build Time",
             "value": 1039,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "d48a2766ccaffa7f283474bdfd204c0612fd7272",
+          "message": "SQUASH ME: First round of review focused on charon.rs; mostly style and comment changes",
+          "timestamp": "2026-05-22T19:11:41Z",
+          "url": "https://github.com/google/zerocopy/pull/3390/commits/d48a2766ccaffa7f283474bdfd204c0612fd7272"
+        },
+        "date": 1779487817376,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Build Time",
+            "value": 1053,
             "unit": "seconds"
           }
         ]
