0a65a662bd2bf9cf93ef75d2eec69f90038bdc1f
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 22:48:55 +0000

    add Docker Build Time (customSmallerIsBetter) benchmark result for f838f8ebae2730fa468f9001522603869c6095aa

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779749333763,
+  "lastUpdate": 1779749335710,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -8018,6 +8018,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "Docker Build Time",
             "value": 6,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "f838f8ebae2730fa468f9001522603869c6095aa",
+          "message": "[anneal][v2] Add and integrate nix-built exocrate",
+          "timestamp": "2026-05-25T20:22:43Z",
+          "url": "https://github.com/google/zerocopy/pull/3383/commits/f838f8ebae2730fa468f9001522603869c6095aa"
+        },
+        "date": 1779749329182,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Build Time",
+            "value": 7,
             "unit": "seconds"
           }
         ]
