28453246784d37643e97537b3e37c060a8d43bb6
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 14:46:44 +0000

    add CI Durations (customSmallerIsBetter) benchmark result for 1f8bc9ee29fb6517819de1d00ce92ca9f29be253

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779719751884,
+  "lastUpdate": 1779720404408,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -12404,6 +12404,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Total CI Duration (All Steps)",
             "value": 1022,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "1f8bc9ee29fb6517819de1d00ce92ca9f29be253",
+          "message": "[anneal][v2] Import vendored cargo dependencies",
+          "timestamp": "2026-05-25T04:56:36Z",
+          "url": "https://github.com/google/zerocopy/pull/3398/commits/1f8bc9ee29fb6517819de1d00ce92ca9f29be253"
+        },
+        "date": 1779720402268,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Pull Time",
+            "value": 88,
+            "unit": "seconds"
+          },
+          {
+            "name": "Test Time",
+            "value": 533,
+            "unit": "seconds"
+          },
+          {
+            "name": "Total CI Duration (All Steps)",
+            "value": 705,
             "unit": "seconds"
           }
         ]
