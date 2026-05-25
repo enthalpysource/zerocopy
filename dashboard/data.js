3bf0ca1a7b651422631ded5484a4bbce01f55323
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 22:48:52 +0000

    add Docker Build Time (customSmallerIsBetter) benchmark result for ebd26a04cf7bed697c35bad7f6feae340fcb07c1

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779749330715,
+  "lastUpdate": 1779749332239,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -7968,6 +7968,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "Docker Build Time",
             "value": 7,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "ebd26a04cf7bed697c35bad7f6feae340fcb07c1",
+          "message": "[anneal][v2] Add Cargo workspace resolution and target resolution logic",
+          "timestamp": "2026-05-25T20:22:43Z",
+          "url": "https://github.com/google/zerocopy/pull/3401/commits/ebd26a04cf7bed697c35bad7f6feae340fcb07c1"
+        },
+        "date": 1779749329121,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Build Time",
+            "value": 6,
             "unit": "seconds"
           }
         ]
