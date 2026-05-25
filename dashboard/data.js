ef2d57e56d56d98629d76d0f9cb9bb084c477a09
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 19:28:03 +0000

    add CI Durations (customSmallerIsBetter) benchmark result for f300c70eb6104a5a175d5c0de463f32de3316d9c

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779737272336,
+  "lastUpdate": 1779737283757,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -14007,6 +14007,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Total CI Duration (All Steps)",
             "value": 719,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "f300c70eb6104a5a175d5c0de463f32de3316d9c",
+          "message": "[anneal][v2] Add and integrate nix-built exocrate",
+          "timestamp": "2026-05-25T04:56:36Z",
+          "url": "https://github.com/google/zerocopy/pull/3383/commits/f300c70eb6104a5a175d5c0de463f32de3316d9c"
+        },
+        "date": 1779737279865,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Pull Time",
+            "value": 85,
+            "unit": "seconds"
+          },
+          {
+            "name": "Test Time",
+            "value": 569,
+            "unit": "seconds"
+          },
+          {
+            "name": "Total CI Duration (All Steps)",
+            "value": 739,
             "unit": "seconds"
           }
         ]
