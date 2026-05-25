94d538711314b8453d2df7d67fb1787df8a7b2bf
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 14:35:51 +0000

    add Docker Build Time (customSmallerIsBetter) benchmark result for 5339ec75605af1b57dbd98733833461fb3ae9025

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779719721014,
+  "lastUpdate": 1779719751884,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -6850,6 +6850,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "Docker Build Time",
             "value": 1062,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "5339ec75605af1b57dbd98733833461fb3ae9025",
+          "message": "[anneal][v2] Add DiagnosticMapper to map compiler errors back to Rust source code",
+          "timestamp": "2026-05-25T04:56:36Z",
+          "url": "https://github.com/google/zerocopy/pull/3403/commits/5339ec75605af1b57dbd98733833461fb3ae9025"
+        },
+        "date": 1779719748829,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Build Time",
+            "value": 1103,
             "unit": "seconds"
           }
         ]
