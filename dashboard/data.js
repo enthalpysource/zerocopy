3a1c8fc6946eec96f5f0f913c92db4f836decd36
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 19:49:12 +0000

    add CI Durations (customSmallerIsBetter) benchmark result for 4f657b10c5c84de1faa6807531050d66fc49930a

--- dashboard/data.js
+++ dashboard/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779738202559,
+  "lastUpdate": 1779738552335,
   "repoUrl": "https://github.com/google/zerocopy",
   "entries": {
     "Docker Image Size": [
@@ -14521,6 +14521,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Total CI Duration (All Steps)",
             "value": 737,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "google",
+            "username": "google"
+          },
+          "committer": {
+            "name": "google",
+            "username": "google"
+          },
+          "id": "4f657b10c5c84de1faa6807531050d66fc49930a",
+          "message": "[anneal][v2][exocrate] Crate-level documentation",
+          "timestamp": "2026-05-25T19:20:57Z",
+          "url": "https://github.com/google/zerocopy/pull/3382/commits/4f657b10c5c84de1faa6807531050d66fc49930a"
+        },
+        "date": 1779738548925,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Docker Pull Time",
+            "value": 89,
+            "unit": "seconds"
+          },
+          {
+            "name": "Test Time",
+            "value": 539,
+            "unit": "seconds"
+          },
+          {
+            "name": "Total CI Duration (All Steps)",
+            "value": 711,
             "unit": "seconds"
           }
         ]
